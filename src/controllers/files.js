8838e04693dd9cba9981e33c8346bc2d536762ad
Kevin Budain <kbudain@universcine.com>
2015-03-22 16:18:31 +0000

    remove files after uploads

--- src/controllers/files.js
+++ src/controllers/files.js
@@ -1,11 +1,15 @@
 var fs = require('fs');
+var unzip = require('unzip');
+var fsExtra = require('fs-extra');
 
 module.exports = {
 
     upload: function (req, res) {
-        fs.readFile(req.files.image.path, function (err, data) {
+        fs.readFile(req.files.zip.path, function (err, data) {
             if (!err) {
-                var newPath = "./uploads/" +   req.files.image.name;
+                var name = req.files.zip.name;
+                var newPath = "./uploads/" + name;
+                var unzipPath = "./uploads/" + name.split('.')[0];
                 fs.writeFile(newPath, data, function (err) {
                     if (err) {
                         res.status(400).json({
@@ -15,6 +19,22 @@ module.exports = {
                         res.status(201).json({
                             'response': "Saved"
                         });
+                        fs.createReadStream(newPath).pipe(unzip.Extract({ path: unzipPath }));
+                        // function for remove files
+                        setTimeout(function () {
+                            fs.unlink(newPath);
+                            if (fsExtra.existsSync(unzipPath)) {
+                                fsExtra.readdirSync(unzipPath).forEach(function (file, index) {
+                                    var curPath = unzipPath + "/" + file;
+                                    if (fsExtra.lstatSync(curPath).isDirectory()) { // recurse
+                                        deleteFolderRecursive(curPath);
+                                    } else { // delete file
+                                        fsExtra.unlinkSync(curPath);
+                                    }
+                                });
+                                fs.rmdir(unzipPath);
+                            }
+                        }, 3000);
                     }
                 });
             } else {
