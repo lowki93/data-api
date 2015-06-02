521b4fd21a4af611df064bb57616ba32fc628853
lowki93 <budain.kevin@yahoo.Fr>
2015-06-02 16:53:19 +0200

    add websocket

--- index.js
+++ index.js
@@ -12,14 +12,47 @@ var app = require('./src/app').app;
 var port = Number(process.env.PORT || 5000);
 var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({ port: 9090 });
+var socketArray = [];
 
 wss.on('connection', function connection(ws) {
+    console.log("connection");
     ws.on('message', function (data) {
+        console.log(socketArray);
         var currentFile = JSON.parse(data);
-        console.log(currentFile);
+        if (currentFile.type === 'desktop') {
+            var userArray = {
+                token : currentFile.token,
+                connected : {
+                    'mobile': false,
+                    'desktop': true
+                },
+                socket: ws
+            };
+            console.log(userArray);
+            socketArray.push(userArray);
+        }
+        if (currentFile.type === 'mobile') {
+
+            var current = null;
+            for (var i = 0; i < socketArray.length; i++) {
+                console.log(socketArray[i], currentFile.token);
+                if( socketArray[i].token == currentFile.token) {
+                    current = socketArray[i];
+                }
+            }
+            console.log(current);
+             if (current != null) {
+                 console.log(current.socket);
+                 current.socket.send("toto");
+             }
+
+            console.log(userArray);
+            //socketArray.push(userArray);
+        }
+        console.log(currentFile.type);
     });
 
-    ws.send({type: "toto", test: 'toto'});
+    //ws.send({type: "toto", test: 'toto'});
 });
 
 app.listen(port);
